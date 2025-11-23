29f4d98f0495e5401335a1d0da11540c9f63c213
sminju <78014608+sminju98@users.noreply.github.com>
2025-11-23 23:59:19 +0900

    feat: Google Tag Manager 추가
    - GTM-58MNB4FJ 추가
    - head와 body에 스크립트 설치
    - 전환 추적 및 분석 준비

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,8 +40,28 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/favicon.svg" />
         <meta name="theme-color" content="#6366F1" />
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-58MNB4FJ');`,
+          }}
+        />
       </head>
       <body className="antialiased min-h-screen">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-58MNB4FJ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
